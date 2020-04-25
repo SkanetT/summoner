e01b9e3be527e7c2e2e5c2957913407efbd0985c
Anton <skanet007@gmail.com>
2020-04-25 18:51:57 +0300

    Initial Commit

---
+++ LoLProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoLProject
+//
+//  Created by Антон on 25.04.2020.
+//  Copyright © 2020 Антон. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
