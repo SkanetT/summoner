c1a42ce356485794dd246a2b6350c6518e7a1be1
Anton <skanet007@gmail.com>
2020-07-14 23:56:19 +0300

    test commit 2

--- LoLProject/Utilis/GlobalConstants.swift
+++ LoLProject/Utilis/GlobalConstants.swift
@@ -10,7 +10,7 @@ class GlobalConstants {
     
     static let shared = GlobalConstants()
     
-    let apiKey = "RGAPI-a2a6f351-8ea3-4423-afc8-870fe3999ffc" /* key for long time */
+    let apiKey = "RGAPI-a2a6f351-8ea3-4423-afc8-870fe3999ffc" /* key for long time 111*/
     
     
     let servers = ["Europe West", "Europe Nordic & East", "Brazil", "Latin America North", "Latin America South", "North America", "Oceania", "Russia", "Turkey", "Japan", "Republic of Korea"]
